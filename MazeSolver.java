a88937152595d7981d483b6eacd794f9fbb07688
Rhiad Ciccoli <who.4004@gmail.com>
2018-06-30 20:08:14 -0300

    Update MazeSolver.java
    Adicionado saida da dead-zone e comentários.

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,9 +1,14 @@
 public class MazeSolver{
-
+	/** A seguinte função recebe como parâmetros o labirinto (uma matriz de caracteres) e as coordenadas x,y de onde se encontra.
+	* o parametro d, pode ser um dos 4 casos: 0 = cima, 1 = direita, 2 = baixo, 3 = esquerda. Este parametro esta ali para garantir
+	* que o algoritmo não fique num loop infinito como subir/descer, esquerda/direita, ou seja, se numa chamada n eu fui para a direita,
+	* numa chamada n+1 não poderei ir para esquerda, evitando loops infinitos. A resolução do mapa é simples, é apenas feita uma verificação
+	* de caminhos vazios ao redor do ponto de cordenada passado, caso exista um ponto maze[x][y] == '', o algoritmo se chama recursivamente
+	* para este ponto */
 	private void solveRecursive(char[][] maze, int x, int y, int d){
 		boolean solved = false;
-		//0=up, 1 = right, 2 = down, 3 = left
-		//a direção passada no 4 parametro serve para o algoritmo não ficar num loop infinito
+		/*o booleano serve para caso o algoritmo chegue num dead-end, ele consiga sair, note que sem o booleano, o algoritmo
+		ficaria preso em um dead-end, caso encontrasse um*/
 		for (int i=0; i<4 && !solved; i++){
 			if(i!=d){	
 				switch(i){
@@ -31,6 +36,36 @@ public class MazeSolver{
 						}	
 				}
 			}	
+		}
+		/*Esta segunda parte do codigo funciona como um escape de uma dead-zone. Caso o algoritmo se encontra em uma, ele 
+		a marcará com um 'x' e saira dela.*/
+		else{
+			maze[y][x] == 'x';
+			switch(d){
+					//up
+					case 0:
+						if (maze[y-1][x] == ' '){
+							solved = solveRecursive(maze, y-1, x, 2);
+							break;
+						}
+					//right	
+					case 1:
+						if(maze[y][x+1] == ' '){
+							solved = solveRecursive(maze, y, x+1, 3);
+							break;
+						}
+					case 2:
+						if(maze[y+1][x] == ' '){
+							solved = solveRecursive(maze, y+1, x, 0);
+							break;
+						}
+					case 3:
+						if(maze[y][x-1] == ' '){
+							solved = solveRecursive(maze, y, x-1, 1);
+							break;
+						}	
+				}
+			 
 		}
 	}
 }
