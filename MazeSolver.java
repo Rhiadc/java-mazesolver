33d2693989ec12a3c4a3ddd712c46f250b9b261e
Rhiad Ciccoli <who.4004@gmail.com>
2018-07-01 23:40:54 -0300

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,12 +1,17 @@
 public class MazeSolver{
+	
 	/** A seguinte função recebe como parâmetros o labirinto (uma matriz de caracteres) e as coordenadas x,y de onde se encontra.
 	* o parametro d, pode ser um dos 4 casos: 0 = cima, 1 = direita, 2 = baixo, 3 = esquerda. Este parametro esta ali para garantir
 	* que o algoritmo não fique num loop infinito como subir/descer, esquerda/direita, ou seja, se numa chamada n eu fui para a direita,
 	* numa chamada n+1 não poderei ir para esquerda, evitando loops infinitos. A resolução do mapa é simples, é apenas feita uma verificação
 	* de caminhos vazios ao redor do ponto de cordenada passado, caso exista um ponto maze[x][y] == '', o algoritmo se chama recursivamente
 	* para este ponto */
+	
+	
 	private static boolean solveRecursive(char[][] maze, int x, int y, int d){
 		boolean solved = false;
+		
+		
 		/*o booleano serve para caso o algoritmo chegue num dead-end, ele consiga sair, note que sem o booleano, o algoritmo
 		ficaria preso em um dead-end, caso encontrasse um*/
 		
@@ -39,6 +44,8 @@ public class MazeSolver{
 						}
 			}
 		}
+		
+		
 		/*Esta segunda parte do codigo funciona como um escape de uma dead-end. Caso o algoritmo se encontra em uma, ele 
 		a marcará com um 'x' e saira dela.*/
 		/*if(!solved){
@@ -69,6 +76,8 @@ public class MazeSolver{
 				}
 			System.out.println("dead-end"); 
 		}*/
+		
+		
 
 		return solved;
 	}
