6faee29ba0f7a17d2c015e0a87f697f7282e363a
Rhiad Ciccoli <who.4004@gmail.com>
2018-07-18 01:57:57 -0300

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -8,16 +8,20 @@ import java.util.List;
 public class Maze{
 
 	//declarar tipos
-	Parede[] paredes;
-	Livre[] livre;
-	char[][] mapaConvertido = new char[10][10];
-	char[][] mapaConvertidoImpressao;
-	List<String> listaConversao; 
+	private Parede parede;
+	private Livre livre;
+	private Saida saida;
+	public Object[][] objeto;
+	private char[][] mapaConvertido;
+	private char[][] mapaConvertidoImpressao;
+	private List<String> listaConversao; 
 	//arquivoMapa é um passado no construtor numero de 0 à 4
-	int arquivoMapa;
+	private int arquivoMapa;
 
-	Public maze(int arquivoMapa){
+	public Maze(int arquivoMapa){
 		this.arquivoMapa = arquivoMapa;
+		this.objeto = new Object[10][10];
+		this.mapaConvertido = new char[10][10];
 	}
 
 	//retorna o numero do mapa
@@ -30,7 +34,8 @@ public class Maze{
 		mapaConvertido = converteMapa(listaConversao);
 	}
 
-
+	//Este metodo le recebe como um parâmetro um valor de 0 a 4, lê o txt do mapa associado ao valor, cria uma lista de strings, e armazena cada
+	//linha como uma unica string
 	private List<String> leMapa(int value){
 		String arquivo = "mapa" + value + ".txt";
 		//falta criar um excp
@@ -51,6 +56,8 @@ public class Maze{
 		return lista;
 	}
 
+	//Este método recebe a lista de strings, lida do txt de mapa, e a converte para uma matriz de char, ignorando os espaços adicionais (retirados da
+	// posição impar)
 	private char[][] converteMapa(List mapa){
 		int cont = 0;
 		for (int i = 0; i <mapa.size(); i++) {
@@ -66,8 +73,30 @@ public class Maze{
 		return mapaConvertido;
 	}
 
+	//Este metodo recebe a matriz em char e cria uma nova matriz de objetos espaço/caminho/final 
+	public Object[][] insere(Livre livre, Parede parede, Saida saida){
+		for(int i=0; i<mapaConvertido.length; i++){
+			for(int j=0; j<mapaConvertido[0].length; j++){
+				if(mapaConvertido[i][j]== ' '){
+					objeto[i][j] = (Livre) livre;
+				}else if(mapaConvertido[i][j] == 'x'){
+					objeto[i][j] = (Parede) parede;
+				}else{
+					objeto[i][j] = (Saida) saida;
+				}
+				
+			}
+		}
+		return objeto;
+	}
+
+	public void imprimeTeste(){
+		System.out.println(objeto.length);
+		System.out.println(objeto[0].length);
+	}
+
 	//acabar esse metodo
-	private char[][] converteMapaSaida(char[][] maze){
+	/*private char[][] converteMapaSaida(char[][] maze){
 		int totalCelulas = 2*maze.length-1;
 		mapaConvertidoImpressao = new char[totalCelulas][totalCelulas];
 		for (int i = 0; i<maze.length; i++){
@@ -79,6 +108,7 @@ public class Maze{
 	}
 
 	private invoca
+	*/
 
 	public void imprimeSolucao(char[][] maze){
 		for (int i = 0; i<maze.length; i++){
